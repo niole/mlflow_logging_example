a17acf5ae9fca8c6d8da658b6ff02acd09b3f6df
niole <niolenelson@gmail.com>
2025-06-10 13:56:01 -0700

    Add custom trace to default

--- default.py
+++ default.py
@@ -1,7 +1,7 @@
 import os
 from random import random, randint
 import time
-from mlflow import log_metric, log_param, log_artifacts
+from mlflow import log_metric, log_param, log_artifacts, MlflowClient
 import mlflow
 from sklearn.model_selection import train_test_split
 from sklearn.datasets import load_diabetes
@@ -11,6 +11,8 @@ mlflow.set_tracking_uri(f"http://localhost:{os.environ['REV_PROXY_PORT']}")
 mlflow.set_experiment("mlflow_default_w_sklearn_"+	str(randint(0, 1000)))
 mlflow.enable_system_metrics_logging()
 mlflow.autolog()
+
+client = MlflowClient()
 
 def main():
     print("Hello from mlflow-test!")
@@ -29,6 +31,34 @@ def main():
     with open("outputs/test.txt", "w") as f:
         f.write("hello world!")
     log_artifacts("outputs")
+
+    # custom trace example
+    root_span = client.start_trace("my_trace")
+
+    # The request_id is used for creating additional spans that have a hierarchical association to this root span
+    request_id = root_span.request_id
+    child_span = client.start_span(
+	name="child_span",
+	request_id=request_id,
+	parent_id=root_span.span_id,
+	inputs={"input_key": "input_value"},
+	attributes={"attribute_key": "attribute_value"},
+    )
+
+    client.end_span(
+	request_id=child_span.request_id,
+	span_id=child_span.span_id,
+	outputs={"output_key": "output_value"},
+	attributes={"custom_attribute": "value"},
+    )
+
+    # End the root span (trace)
+    client.end_trace(
+	request_id=request_id,
+	outputs={"final_output_key": "final_output_value"},
+	attributes={"token_usage": "1174"},
+    )
+
 
     # default autolog example
     db = load_diabetes()
