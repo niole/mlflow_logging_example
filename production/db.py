b4d0e7f26c813c8962e835c8c977fd457e952e5f
niole <niolenelson@gmail.com>
2025-06-24 15:20:55 -0700

    Update the docs added in production

--- production/db.py
+++ production/db.py
@@ -1,3 +1,4 @@
 import chromadb
 chroma_client = chromadb.Client()
 collection = chroma_client.create_collection(name="my_collection")
+collection.add(documents=["this week I have to do DOM-1234", "the president in 20205 is trump"], ids=["domino1234", "whopres2025"])
